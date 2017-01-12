2966371ea8f45e8cf0054d877802939d421b1224
MDRedant <mickey-redant@hotmail.com>
2017-01-12 23:02:25 +0100

    corpus percentage final: creation of bookinfolist

--- Gothic_novel_project/gothicness corpus program.py
+++ Gothic_novel_project/gothicness corpus program.py
@@ -9,22 +9,18 @@ for novel in os.listdir("C:\\Users\\Mickey\\Documents\\Github\\Gothicness-Percen
 		f.close()
 		novellist.append(textnovel)
 		from nltk.corpus import stopwords
-		from nltk.stem.wordnet import WordNetLemmatizer
 		import string
 		stop = set(stopwords.words('english'))
 		exclude = set(string.punctuation + string.digits)
-		name = set(['matilda', 'verezzi', 'zastrozzi', 'guiseppe', 'jerome', 'hippolita', 'manfred', 'frederic', 'bianca', 'isabella', 'theodore', 'glenarvon', 'avondale', 'calantha', 'emmeline', 'godolphin', 'lewis', 'clarke', 'merritt', 'philip', 'ellena', 'schedoni'])
-		lemma = WordNetLemmatizer()
+		name = ['matilda', 'verezzi', 'zastrozzi', 'guiseppe', 'jerome', 'hippolita', 'manfred', 'frederic', 'bianca', 'isabella', 'theodore', 'glenarvon', 'avondale', 'calantha', 'emmeline', 'godolphin', 'lewis', 'clarke', 'merritt', 'philip', 'ellena', 'schedoni', 'vivaldi', 'adeline']
 		def clean(doc):
 			import nltk
 			stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-			punc_free = " ".join(ch for ch in stop_free if ch not in exclude)
-			name_free = " ".join(ft for ft in punc_free if ft not in name)
-			tokens = nltk.word_tokenize(name_free)
+			punc_free = "".join(ch for ch in stop_free if ch not in exclude)
+			tokens = nltk.word_tokenize(punc_free)
 			tagged = nltk.pos_tag(tokens)
 			only_nouns_verbs_and_adjectives = []
 			for word in tagged:
-				word = list(word)
 				if word[1] == 'JJ':
 					only_nouns_verbs_and_adjectives.append(word[0])
 				if word[1] == 'NN':
@@ -35,37 +31,41 @@ for novel in os.listdir("C:\\Users\\Mickey\\Documents\\Github\\Gothicness-Percen
 					only_nouns_verbs_and_adjectives.append(word[0])
 				else:
 					continue
-			return only_nouns_verbs_and_adjectives
+			name_free = [tz for tz in only_nouns_verbs_and_adjectives if tz not in name]
+			return name_free
 		textnovel = clean(textnovel)
 		cleannovellist.append(textnovel)
 import gensim	
 from gensim import corpora
 dictionary = corpora.Dictionary(cleannovellist)
 goth_work_matrix = [dictionary.doc2bow(gothicwork) for gothicwork in cleannovellist]
-Lda = gensim.models.ldamodel.LdaModel(goth_work_matrix, num_topics = 15, id2word = dictionary, passes = 20, minimum_probability = 0 )
+Lda = gensim.models.ldamodel.LdaModel
+Ldamodel = Lda(goth_work_matrix, num_topics = 10, id2word = dictionary, passes = 5, minimum_probability = 0 )
+print(Ldamodel.print_topics(num_topics = 10, num_words= 100))
+distr_of_topics_per_book = []
 for i in range(len(cleannovellist)):
-	distr_of_topics_per_book = Lda[goth_work_matrix[i]]
+	distr_of_topics_per_book.append(Ldamodel[goth_work_matrix[i]])
 percentages = []
 booknumber = 0
-for book in distr_of_topics:
+for book in distr_of_topics_per_book:
 	gothicness = 0
 	for theme in book:
+		print(theme)
 		for i in str(theme[0]):
-			gothicness += theme[1]
+			gothicness += (theme[1]/10)
 	booknumber += 1
-	gothicnesspercentage = booknumber , (gothicness*100)
+	gothicnesspercentage = booknumber , (gothicness*1000)
 	percentages.append(gothicnesspercentage)
 bookinformation = []
 for novel in novellist:
 	informationlist = []
-	for text in novel:
-		text = text.split('\n')
-		name = text[0]
-		author = text[2]
-		publication_date = text[4]
-		informationlist.append(name)
-		informationlist.append(author)
-		informationlist.append(publication_date)
+	text = novel.split('\n')
+	name = text[0]
+	author = text[2]
+	publication_date = text[4]
+	informationlist.append(name)
+	informationlist.append(author)
+	informationlist.append(publication_date)
 	bookinformation.append(informationlist)
 bookdatalist = list(zip(bookinformation, percentages))
 everything_equallist = []
@@ -78,9 +78,10 @@ for book in bookdatalist:
 	if book[-1]:
 		togetherlist.append(book[1][1])
 	everything_equallist.append(togetherlist)
+print(everything_equallist)
 dob = open('bookinfolist.txt', 'wt', encoding = 'utf-8')
 for item in everything_equallist:
-	dob.write("%\n" % item)
+	dob.write("\n" + str(item))
 dob.close()
 
 #ldamodel = Lda(goth_work_matrix, num_topics=10, id2word = dictionary, passes = 20)
